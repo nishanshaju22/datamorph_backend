c8a57219059f3f58d36ecfff3e73c256132e53c8
nishanshaju22 <nishan.shaju@icloud.com>
2026-07-04 11:22:29 +1000

    health route added

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
+    path("health/", health),
     path('admin/', admin.site.urls),
     path("api/uploads/", include("uploads.urls")),
     path("api/jobs/", include("jobs.urls")),
